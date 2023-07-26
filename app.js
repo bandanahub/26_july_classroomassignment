d658530dad5da1ca20ff6a78826ad784bf0909f4
bandanahub <138704329+bandanahub@users.noreply.github.com>
2023-07-26 13:24:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-function calcualte (num1 , numb2 , operator){
-    switch (operator){
-        case '+' :
-            return num1 + numb2
-        case '-':
-            return num1 - numb2;
-        case '*':
-             return num1 * numb2;
-        case '/':
-            if (num2 != 0){
-            return num1/num2;
-            }else {
-                return 'error : unknown operator,';
-            }
+function calculate() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var operator = document.getElementById("operator").value;
+    var result;
+  
+    switch (operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        if (num2 !== 0) {
+          result = num1 / num2;
+        } else {
+          result = 'Error: Division by zero';
+        }
+        break;
+      default:
+        result = 'Error: Unknown operator';
     }
-}
-
-$(document).ready(function() {
+  
+    var resultDiv = document.getElementById("result");
+    resultDiv.textContent = "Result: " + result;
+  }
+  
+  $(document).ready(function() {
     $("#myButton").click(function() {
-    alert("Button clicked!");
+      calculate();
     });
-    });
+  });
+  
